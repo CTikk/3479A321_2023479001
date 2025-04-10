66dda78ac0d9e5b13cf70f19b16814e848314481
Valentine Sierra <valesierracuevas@gmail.com>
2025-04-10 19:16:22 -0400

    Agregados botones al pie de pantalla

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.tealAccent),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Laboratorio 4 ^^'),
     );
   }
 }
@@ -56,6 +56,22 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  // a. función para disminuir el contador:
+  void _decrementCounter()
+  {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  // b. función para reiniciar el contador
+  void _resetCounter()
+  {
+    setState(() {
+      _counter = 0;
+    });
+  }
+
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -67,44 +83,19 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  // c - f reemplazo del botón flotante por una barra persistente en el pie:
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+ Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text('Haz presionado el botón estas veces:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
@@ -112,11 +103,41 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.air),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // c. Botones persistentes al pie de pantalla usando bottomNavigationBar
+      bottomNavigationBar: buildBottomAppBar(),
+    );
+  }
+
+  BottomAppBar buildBottomAppBar() {
+    return BottomAppBar(
+      color: Colors.grey.shade200,
+      shape: const CircularNotchedRectangle(),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            // d. Botón para restar
+            IconButton(
+              icon: const Icon(Icons.remove),
+              tooltip: 'Disminuir',
+              onPressed: _decrementCounter,
+            ),
+            // f. Botón para reiniciar
+            IconButton(
+              icon: const Icon(Icons.restart_alt_rounded),
+              tooltip: 'Reiniciar',
+              onPressed: _resetCounter,
+            ),
+            // e. Botón para sumar
+            IconButton(
+              icon: const Icon(Icons.add),
+              tooltip: 'Incrementar',
+              onPressed: _incrementCounter,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
